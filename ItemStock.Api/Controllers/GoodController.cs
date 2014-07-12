36144c2ce0975d10c6cc57a76e7d44f7a71f58eb
mahathir <ai_mahathir@yahoo.com>
2014-07-12 13:05:51 +0700

    Fixing Build Error

--- ItemStock.Api/Controllers/GoodController.cs
+++ ItemStock.Api/Controllers/GoodController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 using ItemStock.DTO.Implementation;
 using ItemStock.DTO.Interface;
 using ItemStock.Repository.Interface;
@@ -21,22 +22,40 @@ namespace ItemStock.Api.Controllers
             _goodRepository = goodRepository;
         }
 
-        public async Task<ICollection<Good>> Get()
+        [ResponseType(typeof(ICollection<Good>))]
+        public async Task<IHttpActionResult> Get()
         {
-            return _goodRepository.FindAll().Cast<Good>().ToList();
+            return Ok(_goodRepository.FindAll().Cast<Good>().ToList());
         }
 
-        public async Task<Good> Get(Guid id)
+        [ResponseType(typeof(Good))]
+        public async Task<IHttpActionResult> Get(Guid id)
         {
-            return _goodRepository.Find(id) as Good;
+            return Ok(_goodRepository.Find(id) as Good);
         }
 
-        public async Task Put(Good good)
+        [ResponseType(typeof(Good))]
+        public async Task<IHttpActionResult> Put(Good good)
         {
+            if(good == null || good.Id == new Guid())
+            {
+                return BadRequest();
+            }
+
+            var existingGood = _goodRepository.Find(good.Id);
+
+            if(existingGood == null)
+            {
+                return BadRequest();
+            }
+
             _goodRepository.Update(good);
+
+            return Ok<Good>(good);
         }
 
-        public async Task Post(Good good)
+        [ResponseType(typeof(Good))]
+        public async Task<IHttpActionResult> Post(Good good)
         {
             var user = new AppUser { Id = Guid.NewGuid() };
 
@@ -46,12 +65,22 @@ namespace ItemStock.Api.Controllers
             good.CreatedDateTime = DateTime.Now;
 
             _goodRepository.Add(good);
+
+            return Ok<Good>(good);
         }
 
-        public async Task Delete(Guid id)
+        public async Task<IHttpActionResult> Delete(Guid id)
         {
             var entity = _goodRepository.Find(id);
-            if (entity != null) _goodRepository.Delete(entity);
+            if (entity != null)
+            {
+                _goodRepository.Delete(entity);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- ItemStock.Api/Controllers/IBaseController.cs
+++ ItemStock.Api/Controllers/IBaseController.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
 
 namespace ItemStock.Api.Controllers
 {
     public interface IBaseController<Entity>
         where Entity : class
     {
-        Task<ICollection<Entity>> Get();
+        Task<IHttpActionResult> Get();
 
-        Task<Entity> Get(Guid id);
+        Task<IHttpActionResult> Get(Guid id);
 
-        Task Put(Entity entity);
+        Task<IHttpActionResult> Put(Entity entity);
 
-        Task Post(Entity entity);
+        Task<IHttpActionResult> Post(Entity entity);
 
-        Task Delete(Guid id);
+        Task<IHttpActionResult> Delete(Guid id);
     }
 }
