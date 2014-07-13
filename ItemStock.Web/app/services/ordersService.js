29b7bfc11a3d8983a822961fb95299c87ee98081
mahathir <ai_mahathir@yahoo.com>
2014-07-13 14:52:28 +0700

    Adding SPA using AngularJs

---
+++ ItemStock.Web/app/services/ordersService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('ordersService', ['$http', function ($http) {
+
+    var serviceBase = 'http://localhost:1036/';
+    var ordersServiceFactory = {};
+
+    var _getOrders = function () {
+
+        return $http.get(serviceBase + 'api/good').then(function (results) {
+            return results;
+        });
+    };
+
+    ordersServiceFactory.getOrders = _getOrders;
+
+    return ordersServiceFactory;
+
+}]);
